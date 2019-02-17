5204517788e62f8904dd22ea4f30da713cbdc3a4
zuhern <eyajiya@gmail.com>
2019-02-17 21:29:28 +0900

    json 데이터로 관리하기 위한 준비 중

--- src/data/data.js
+++ src/data/data.js
@@ -2,16 +2,5 @@ export default {
   menuList: [
     {path: 'service', name: 'IMEZ Service', id: 'service'},
     {path: 'partner', name: 'IMEZ Partner', id: 'partner'}
-  ],
-  langList: [
-    {
-      firstDiv: 'COMMON',
-      secondDiv: 'BUTTON',
-      thirdDiv: 'OK',
-      korLang: '확인',
-      engLang: 'ok',
-      jpLang: 'okjp',
-      cnLang: 'okcn'
-    }
   ]
 };
